b68b0e317b8465dd22c8d659a05ee0fc800ca159
ahlim3 <78980758+ahlim3@users.noreply.github.com>
2022-01-21 20:18:26 -0500

    Initial Commit

---
+++ Shared/_dMCparticleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _dMCparticleApp.swift
+//  Shared
+//
+//  Created by anthony lim on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct _dMCparticleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
